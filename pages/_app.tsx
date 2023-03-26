21e468fb10a5474d8b4b049ee60c17a1af07876d
Leah Vashevko <leah@vashevko.com>
2023-03-26 10:38:42 -0400

    Change logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,27 +31,27 @@ function App({ Component, pageProps }: AppProps) {
                 <link
                     rel="apple-touch-icon"
                     sizes="180x180"
-                    href="/apple-touch-icon.png?v=1"
+                    href="/apple-touch-icon.png?v=2"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="32x32"
-                    href="/favicon-32x32.png?v=1"
+                    href="/favicon-32x32.png?v=2"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="16x16"
-                    href="/favicon-16x16.png?v=1"
+                    href="/favicon-16x16.png?v=2"
                 />
-                <link rel="manifest" href="/site.webmanifest?v=1" />
+                <link rel="manifest" href="/site.webmanifest?v=2" />
                 <link
                     rel="mask-icon"
-                    href="/safari-pinned-tab.svg?v=1"
+                    href="/safari-pinned-tab.svg?v=2"
                     color="#00919e"
                 />
-                <link rel="icon" href="/favicon.ico?v=1" />
+                <link rel="icon" href="/favicon.ico?v=2" />
                 <meta name="msapplication-TileColor" content="#00919e" />
                 <meta name="theme-color" content="#00919e" />
             </Head>
